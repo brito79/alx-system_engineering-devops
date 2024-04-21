1b9b6bb3b1bc858af293dbf851a09ce24e92eaaf
Brito79 <bshayamano2002@gmail.com>
2024-04-21 14:09:12 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# Using Puppet, install puppet-lint. ensure present or latest but we want 2.1.1
-package{
-  'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem'
-}
+# install puppet-lint -v 2.5.0
 
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+}
